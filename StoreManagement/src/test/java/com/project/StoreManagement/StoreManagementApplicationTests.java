a49c62fb23fa9fb998b8b4b828a01ff3b363de8d
YessikaMorantes <yessikamorantes@wposs.com>
2024-04-03 17:51:47 -0500

    PROYECTO EN BLANCO
    Se crea proyecto de sprintboot en blanco.

---
+++ StoreManagement/src/test/java/com/project/StoreManagement/StoreManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.StoreManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
