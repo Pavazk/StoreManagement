29f0553c879e541ec2b2eec83b443d1cd599316b
Luis_Ospina_Pava <luisospina@wposs.com>
2024-04-06 18:30:02 -0500

    DOCUMENTACION
    Se documenta codigo de la implementacion del servicio

--- StoreManagement/src/main/java/com/project/StoreManagement/services/ArticleServicesImplement.java
+++ StoreManagement/src/main/java/com/project/StoreManagement/services/ArticleServicesImplement.java
@@ -13,25 +13,37 @@ import java.util.Optional;
 public class ArticleServicesImplement implements ArticleServices {
     @Autowired
     private ArticleRepository articleRepository;
+
+    /**
+     * Metodo encargado de almacenar el articulo en la base de datos
+     * @param article
+     * @return articulo creado
+     */
     @Override
     public Article createArticle(Article article) {
         return articleRepository.save(article);
     }
 
+    /**
+     * Metodo encargado de buscar el articulo por su id
+     * @param id
+     * @return articulo con el id correspondiente o null en caso de no encontrar ninguno
+     */
     @Override
     public Article getArticleById(Long id) {
         Optional<Article> articleById = articleRepository.findById(id);
-        if (articleById.isEmpty()){
-            return null;
-        }
-
-        return articleById.get();
+        return articleById.orElse(null);
     }
 
+    /**
+     * Metodo encargado de actualizar el articulo que corresponda con el id
+     * @param updatedArticle
+     * @param id
+     * @return arrticulo actualizado o null en caso de no encontrar articulo con el id correspondiente
+     */
     @Override
     public Article updateArticle(Article updatedArticle, Long id) {
         Optional<Article> optionalArticle = articleRepository.findById(id);
-
         if (optionalArticle.isPresent()) {
             Article existingArticle = optionalArticle.get();
             existingArticle.setArticleDescription(updatedArticle.getArticleDescription());
@@ -42,6 +54,10 @@ public class ArticleServicesImplement implements ArticleServices {
         }
     }
 
+    /**
+     * Metodo encargado de retornar todos los articulos
+     * @return lista de articulos almacenados en la base de datos
+     */
     @Override
     public List<Article> getAllArticles() {
         return (List<Article>) articleRepository.findAll();
