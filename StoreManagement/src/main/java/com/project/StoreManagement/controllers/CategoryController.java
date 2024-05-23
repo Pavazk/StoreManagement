f2747d39115c7dd213383740d7be4943ff4936d1
Yessika Morantes <ykpaola18@gmail.com>
2024-05-22 23:41:03 -0500

    RESPONSE Y REQUEST CATEGORY
    - Se configura el request y el response para las metodos del Crud para categoria

--- StoreManagement/src/main/java/com/project/StoreManagement/controllers/CategoryController.java
+++ StoreManagement/src/main/java/com/project/StoreManagement/controllers/CategoryController.java
@@ -1,6 +1,8 @@
 package com.project.StoreManagement.controllers;
 
 import com.project.StoreManagement.models.Category;
+import com.project.StoreManagement.models.RequestMessage;
+import com.project.StoreManagement.models.ResponseMessage;
 import com.project.StoreManagement.services.CategoryServices;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,23 +18,28 @@ public class CategoryController {
     private CategoryServices categoryServices;
 
     @PostMapping
-    public Category createCategory(@RequestBody @Valid Category category) {
+    public ResponseMessage createCategory(@RequestBody @Valid RequestMessage<Category> category) {
         return categoryServices.createCategory(category);
     }
 
     @PutMapping({"/{id}"})
-    public Category getCategoryById(@PathVariable @Valid Long id) {
+    public ResponseMessage getCategoryById(@PathVariable @Valid Long id) {
         return categoryServices.getCategoryById(id);
     }
 
     @PostMapping({"/{id}"})
-    public Category updateCategory(@RequestBody @Valid Category article, @PathVariable Long id) {
-        return categoryServices.updateCategory(article, id);
+    public ResponseMessage updateCategory(@RequestBody @Valid RequestMessage<Category> category, @PathVariable Long id) {
+        return categoryServices.updateCategory(category, id);
     }
 
     @GetMapping
     public List<Category> updateCategory() {
         return categoryServices.getAllCategory();
+    }
+
+    @DeleteMapping({"/{id}"})
+    public ResponseMessage deleteArticle(@PathVariable Long id) {
+        return categoryServices.deleteCategory(id);
     }
 
 }
--- StoreManagement/src/main/java/com/project/StoreManagement/services/CategoryServicesImplement.java
+++ StoreManagement/src/main/java/com/project/StoreManagement/services/CategoryServicesImplement.java
@@ -1,8 +1,12 @@
 package com.project.StoreManagement.services;
 
+import com.project.StoreManagement.models.Article;
 import com.project.StoreManagement.models.Category;
+import com.project.StoreManagement.models.RequestMessage;
+import com.project.StoreManagement.models.ResponseMessage;
 import com.project.StoreManagement.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,7 +14,7 @@ import java.util.Optional;
 
 @Service
 public class CategoryServicesImplement implements CategoryServices {
-
+    ArticleServicesImplement articleServicesImplement = new ArticleServicesImplement();
     @Autowired
     private CategoryRepository categoryRepository;
 
@@ -20,8 +24,9 @@ public class CategoryServicesImplement implements CategoryServices {
      * @return la categoria anteriormente creada
      */
     @Override
-    public Category createCategory(Category category) {
-        return categoryRepository.save(category);
+    public ResponseMessage createCategory(RequestMessage<Category> category) {
+        categoryRepository.save(category.getObject());
+        return articleServicesImplement.createResponse("Categoria creada correctamente", HttpStatus.OK);
     }
 
     /**
@@ -30,8 +35,13 @@ public class CategoryServicesImplement implements CategoryServices {
      * @return la categoria con el id correspondiente o null en caso de no encontrar ninguno
      */
     @Override
-    public Category getCategoryById(Long id) {
-        return categoryRepository.findById(id).orElse(null);
+    public ResponseMessage getCategoryById(Long id) {
+        Optional<Category> categoryById = categoryRepository.findById(id);
+        if (categoryById.isPresent()){
+            return articleServicesImplement.createResponse("Categoria " + categoryById.get().getCategoryName() + " se encontro correctamente", HttpStatus.OK);
+        } else {
+            return articleServicesImplement.createResponse("No existe categoria con ese id", HttpStatus.NOT_FOUND);
+        }
     }
 
     /**
@@ -41,19 +51,20 @@ public class CategoryServicesImplement implements CategoryServices {
      * @return categoria actualizada o null en caso de no encontrar categoria con el id correspondiente
      */
     @Override
-    public Category updateCategory(Category newCategory, Long id) {
+    public ResponseMessage updateCategory(RequestMessage<Category> newCategory, Long id) {
         Optional<Category> optionalCategory = categoryRepository.findById(id);
         if (optionalCategory.isPresent()) {
             Category oldCategory = optionalCategory.get();
-            if (newCategory.getCategoryName() != null) {
-                oldCategory.setCategoryName(newCategory.getCategoryName());
+            if (newCategory.getObject().getCategoryName() != null) {
+                oldCategory.setCategoryName(newCategory.getObject().getCategoryName());
             }
-            if (newCategory.getCategoryDescription() != null) {
-                oldCategory.setCategoryDescription(newCategory.getCategoryDescription());
+            if (newCategory.getObject().getCategoryDescription() != null) {
+                oldCategory.setCategoryDescription(newCategory.getObject().getCategoryDescription());
             }
-            return categoryRepository.save(oldCategory);
+            categoryRepository.save(oldCategory);
+            return articleServicesImplement.createResponse("Articulo con id: " + optionalCategory.get().getId() + " correctamente actualizado", HttpStatus.OK);
         } else {
-            return null;
+            return articleServicesImplement.createResponse("No se encontro el articulo a actualizar", HttpStatus.NOT_FOUND);
         }
     }
 
@@ -64,5 +75,17 @@ public class CategoryServicesImplement implements CategoryServices {
     @Override
     public List<Category> getAllCategory() {
         return (List<Category>) categoryRepository.findAll();
+    }
+
+    @Override
+    public ResponseMessage deleteCategory(Long id) {
+        Optional<Category> optionalArticle = categoryRepository.findById(id);
+
+        if (optionalArticle.isPresent() && optionalArticle.get().getListArticle().isEmpty()) {
+            categoryRepository.delete(optionalArticle.get());
+            return articleServicesImplement.createResponse("Articulo con id: " + optionalArticle.get().getId() + " eliminado correctamente", HttpStatus.OK);
+        } else {
+            return articleServicesImplement.createResponse("Articulo a eliminar no encontrado", HttpStatus.NOT_FOUND);
+        }
     }
 }
